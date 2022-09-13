e5d9830a34c656aa1f0bb8f914181f2f62dad642
Zun Pan Phyu <zun.p.phyu@frontiir.net>
2022-09-13 17:00:15 +0630

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,161 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <h2>Employee Directory</h2>
+    <TableView
+      :headers="columns"
+      :rows="userInfo"
+      :sort="sort1"
+    >
+      <template v-slot:items="{ row }">
+        <td>{{ row.name }}</td>
+        <td>{{ row.id }}</td>   
+        <td>{{ row.email }}</td>              
+        <td>{{ row.gender }}</td>     
+        <td>{{ row.status }}</td>     
+      </template>
+
+      <template v-slot:no-data>
+        <span>No data</span>
+      </template>
+    </TableView>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import TableView from './TableView.vue'
+import { columns } from '@/data'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    TableView,
+  },
   props: {
     msg: String
-  }
+  },
+   data() {
+    return {
+      columns: columns,
+      userInfo: [], 
+      sort1:{
+        field: 'name',
+        order: 'asc'
+      },    
+      pagination:{
+        itemsPerPage: 7,
+        align: 'center',
+        visualStyle: 'select'
+      },
+    };
+  },
+
+    methods: {
+    async fetctUserFacts() {
+    const infoResponse = await axios.get(
+      "https://gorest.co.in/public/v2/users"
+    );
+    this.userInfo = infoResponse.data;
+    console.log("data", infoResponse.data);
+  },
+    },
+    async mounted() {
+      await this.fetctUserFacts();
+    },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss">
+@import url(https://fonts.googleapis.com/css?family=Roboto+Mono);
+
+body, html{
+  height: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#app {
+  font-family: 'Roboto Mono', Helvetica, Arial, sans-serif;
+  font-size: 15px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 100%;
+  margin: 0 auto;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+h1{
+  margin-bottom: 2em;
+  color: #f90;
 }
-a {
-  color: #42b983;
+.separator{
+  margin: 2em 0;
+  text-align: center;
+}
+.custom-style {
+  // Table styles
+  .ozn-table {
+    border-collapse: collapse;
+    width: 100%;
+    thead {
+      th { 
+        border-bottom: 1px solid #ffffff;
+        padding: 0 10px;
+        height: 48px;
+        text-align: left; 
+        font-size: 1em; 
+        color: #fff;
+        background-color: #7cc3fd; 
+        cursor: pointer; 
+        &:hover{
+          span{
+            text-decoration: underline;
+            text-decoration-style: dotted;
+          }
+        }        
+        i{
+          color: #ffffff; 
+          &.active{
+            color: #ff00aa;
+            + span{
+              color: #a9237c;
+            }
+          }
+        }       
+      }
+    }
+    tbody {
+      tr{
+        &:nth-child(odd){
+          background-color: #e9f5ff;
+        }
+        &:nth-child(even){
+          background-color: #fafaeb;
+        }
+      }
+      td {
+        border-bottom: 1px solid #ffffff;
+        padding: 0 10px;
+        height: 48px;
+        font-size: 1em; 
+      }
+    }
+  }
+  // Paginator styles    
+  .ozn-paginator{
+    margin-top: .5em;
+    select{
+      border: 1px solid #7cc3fd;
+      border-radius: 8px;
+      color: #ffffff;
+      background-color:  #7cc3fd;
+      outline: none;
+    }
+    button{
+      border: 1px solid #7cc3fd;
+      border-radius: 8px;
+      color: #ffffff;
+      background-color:  #7cc3fd;
+      outline: none;
+    }    
+  }
+
 }
 </style>
---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const columns = [
+    {"label":"Employees","field":"name","sortable":true,"type":"String"},
+    {"label":"ID","field":"id","sortable":true,"type":"Number"},
+    {"label":"Email","field":"email","sortable":true,"type":"String"},
+    {"label":"Gender","field":"gender","sortable":true,"type":"String"},
+    {"label":"Status","field":"status","sortable":true,"type":"String"},
+    ]
